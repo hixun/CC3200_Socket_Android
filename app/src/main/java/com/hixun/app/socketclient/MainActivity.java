24679626874b3cb3f6630c253191aa2bcc5777e6
hixun <hehaixunde@gmail.com>
2017-03-03 08:53:55 +0800

    can draw wave.

--- app/src/main/java/com/hixun/app/socketclient/MainActivity.java
+++ app/src/main/java/com/hixun/app/socketclient/MainActivity.java
@@ -1,5 +1,9 @@
 package com.hixun.app.socketclient;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.io.BufferedReader;
@@ -14,12 +18,10 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.json.JSONObject;
-
-//import com.tencent.stat.MtaSDkException;
-//import com.tencent.stat.StatConfig;
-//import com.tencent.stat.StatService;
 
 import android.R.integer;
 import android.os.AsyncTask;
@@ -27,6 +29,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -36,8 +40,23 @@ import android.widget.Toast;
 import android.app.Activity;
 
 public class MainActivity extends Activity implements OnClickListener{
+
+    private SurfaceHolder holder;
+    private Paint paint;
+    final int HEIGHT=540;
+    final int WIDTH=1280;
+    final int X_OFFSET = 5;
+    private int cx = X_OFFSET;
+
+    public int rec_data = 10;
+
+    //实际的Y轴的位置
+    int centerY = HEIGHT /2;
+    Timer timer = new Timer();
+    TimerTask task = null;
+
     private EditText mIPEdt, mPortEdt,  mMessageEdt;
-    private static TextView mConsoleTxt,mTest;
+    private static TextView mConsoleTxt;
 
     private static StringBuffer mConsoleStr = new StringBuffer();
     private Socket mSocket;
@@ -52,6 +71,23 @@ public class MainActivity extends Activity implements OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+
+        holder.addCallback(new SurfaceHolder.Callback() {
+            public void surfaceChanged(SurfaceHolder holder,int format,int width,int height){
+                drawBack(holder);
+            }
+
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                // TODO Auto-generated method stub
+                timer.cancel();
+            }
+        });
     }
 
     private void initView() {
@@ -59,10 +95,19 @@ public class MainActivity extends Activity implements OnClickListener{
         mPortEdt = (EditText) findViewById(R.id.port_edt);
         mMessageEdt = (EditText) findViewById(R.id.msg_edt);
         mConsoleTxt = (TextView) findViewById(R.id.receive_txt);
-        mTest = (TextView)findViewById(R.id.test);
         findViewById(R.id.start_btn).setOnClickListener(this);
         findViewById(R.id.send_btn).setOnClickListener(this);
-        findViewById(R.id.clear_btn).setOnClickListener(this);
+        findViewById(R.id.sin).setOnClickListener(this);
+
+
+        final SurfaceView surface = (SurfaceView)findViewById(R.id.show);
+        //初始化SurfaceHolder对象
+        holder = surface.getHolder();
+        //初始化画笔paint
+        paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(3);
+
         mHandler = new SocketHandler();
     }
 
@@ -107,14 +152,62 @@ public class MainActivity extends Activity implements OnClickListener{
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(final View v) {
         switch (v.getId()) {
             case R.id.send_btn:
                 send();
+                drawBack(holder);
+                cx = X_OFFSET;
+                if(task != null){
+                    task.cancel();
+                }
+
+                task = new TimerTask() {
+
+                    @Override
+                    public void run() {
+//                        int cy = v.getId() == R.id.sin ? centerY -(int)(100 * Math.sin((cx -5) *2 * Math.PI/150)):
+//                                centerY -(int)(100 * Math.cos((cx-5)*2*Math.PI/150));
+                        int cy = v.getId() == R.id.sin ? centerY -(int)(100 * Math.sin((cx -5) *2 * Math.PI/150)):
+                                centerY -(int)(100 * Math.cos((cx-5)*2*Math.PI/150));
+//                        int cy = 20;
+                        Canvas canvas = holder.lockCanvas(new Rect(cx,cy-2,cx+2,cy+2));
+                        canvas.drawPoint(cx, cy, paint);
+                        cx++;
+                        if(cx >WIDTH){
+                            task.cancel();
+                            task = null;
+                        }
+                        holder.unlockCanvasAndPost(canvas);
+                    }
+                };
+                timer.schedule(task, 0,30);
                 break;
-            case R.id.clear_btn:
-                mConsoleStr.delete(0, mConsoleStr.length());
-                mConsoleTxt.setText(mConsoleStr.toString());
+            case R.id.sin:
+                drawBack(holder);
+                cx = X_OFFSET;
+                if(task != null){
+                    task.cancel();
+                }
+
+                task = new TimerTask() {
+
+                    @Override
+                    public void run() {
+                        int cy = v.getId() == R.id.sin ? centerY -(int)(100 * Math.sin((cx -5) *2 * Math.PI/150)):
+                                centerY -(int)(100 * Math.cos((cx-5)*2*Math.PI/150));
+//                        int cy = 20;
+                        Canvas canvas = holder.lockCanvas(new Rect(cx,cy-2,cx+2,cy+2));
+                        canvas.drawPoint(cx, cy, paint);
+                        cx++;
+                        if(cx >WIDTH){
+                            task.cancel();
+                            task = null;
+                        }
+                        holder.unlockCanvasAndPost(canvas);
+                    }
+                };
+                timer.schedule(task, 0,30);
                 break;
             case R.id.start_btn:
                 if(!isStartRecieveMsg) {
@@ -154,6 +247,8 @@ public class MainActivity extends Activity implements OnClickListener{
 
     static class SocketHandler extends Handler {
 
+        private int rec_data;
+
         @Override
         public void handleMessage(Message msg) {
             // TODO Auto-generated method stub
@@ -161,7 +256,8 @@ public class MainActivity extends Activity implements OnClickListener{
             switch (msg.what) {
                 case 0:
                     try {
-                        mConsoleStr.append(msg.obj);
+                       rec_data = Integer.parseInt(msg.obj.toString());
+                        mConsoleStr.append(msg.obj + "  ");
                         mConsoleTxt.setText(mConsoleStr);
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -181,167 +277,20 @@ public class MainActivity extends Activity implements OnClickListener{
         isStartRecieveMsg = false;
     }
 
-}
+    private void drawBack(SurfaceHolder holder){
+        Canvas canvas = holder.lockCanvas();
+        //绘制白色背景
+        canvas.drawColor(Color.BLACK);
+        Paint p = new Paint();
+        p.setColor(Color.GREEN);
+        p.setStrokeWidth(2);
 
-//public class MainActivity extends AppCompatActivity implements Runnable{
-//
-//    private EditText mIPEdt, mPortEdt, mMessageEdt;  //编辑框，分别为IP，端口号，和发送数据框
-//    private static TextView mReceiveTxt, mTest;  //文本框，接收的信息内容和test用
-//    private Button send_btn;
-//
-//    private Socket mSocket = null;
-//    protected BufferedReader mReader;//BufferedWriter 用于接收消息
-//    protected PrintWriter mWriter;//BufferedReader 用于发送消息
-//
-//    private String once_Msg = ""; //每次接收的数据
-//    private StringBuilder receive_show = null;  //接收的总的数据
-//
-//    private void initView() {
-//        receive_show = new StringBuilder();
-//        mIPEdt = (EditText) findViewById(R.id.ip_edt);
-//        mPortEdt = (EditText) findViewById(R.id.port_edt);
-//        mMessageEdt = (EditText) findViewById(R.id.msg_edt);
-//        mReceiveTxt = (TextView) findViewById(R.id.receive_txt);
-//        mTest = (TextView) findViewById(R.id.test);
-//
-//        send_btn = (Button)findViewById(R.id.send_btn);
-//
-////        findViewById(R.id.start_btn).setOnClickListener(this);
-////        findViewById(R.id.send_btn).setOnClickListener(this);
-////        findViewById(R.id.clear_btn).setOnClickListener(this);
-//    }
-//
-//    //定义一个handler对象,用来刷新界面
-//    public Handler handler = new Handler() {
-//        public void handleMessage(Message msg) {
-//            if (msg.what == 0x123) {
-//                receive_show.append(once_Msg);
-//                mReceiveTxt.setText(receive_show.toString());
-//            }
-//        };
-//    };
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        initView();
-////        Button send_btn = (Button) findViewById(R.id.send_btn);
-////        send_btn.setOnClickListener(this);
-//        //当程序一开始运行的时候就实例化Socket对象,与服务端进行连接,获取输入输出流
-//        //因为4.0以后不能再主线程中进行网络操作,所以需要另外开辟一个线程
-//        new Thread() {
-//
-//            public void run() {
-//                try {
-//                    String ip = mIPEdt.getText().toString();//得到界面输入的IP地址
-//                    int port = Integer.parseInt(mPortEdt.getText().toString());//得到界面输入的Socket端口号
-//                    mSocket = new Socket(ip, port);
-//                    mReader = new BufferedReader(new InputStreamReader(mSocket.getInputStream(), "UTF-8"));
-//                    mWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
-//                            mSocket.getOutputStream())), true);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }.start();
-//        //为发送按钮设置点击事件
-//        send_btn.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//        String msg = mMessageEdt.getText().toString();
-//        if (mSocket.isConnected()) {
-//            if (!mSocket.isOutputShutdown()) {
-//                mWriter.println(msg);
-//            }
-//            try {
-//                Thread.sleep(50);
-//                receive_show.append("Send Message");
-//                mReceiveTxt.setText(receive_show.toString());
-//
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//
-//        }
-//            }
-//        });
-//        new Thread( MainActivity.this).start();
-//    }
-//
-//    //重写run方法,在该方法中输入流的读取
-//    @Override
-//    public void run() {
-//        try {
-//            while (true) {
-//                    if (mSocket.isConnected()) {
-//
-//                        receive_show.append("mSocket.isConnected");
-//                        mReceiveTxt.setText(receive_show.toString());
-//
-//                        if (!mSocket.isInputShutdown()) {
-//
-//                            receive_show.append("mSocket isInputShutdown");
-//                            mReceiveTxt.setText(receive_show.toString());
-//
-//                            if ((once_Msg = mReader.readLine()) != null) {
-//                                once_Msg += "\n";
-//                                handler.sendEmptyMessage(0x123);
-//                            }
-//                        }
-//                    }
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-////    @Override
-////    public void onClick(View v) {
-////        new Thread() {
-////            @Override
-////            public void run() {
-////                try {
-////                    acceptServer();
-////                } catch (IOException e) {
-////                    e.printStackTrace();
-////                }
-////            }
-////        }.start();
-////    }
-//
-//
-//
-//    //    @Override
-////    public void onClick(View v) {
-////        switch (v.getId()) {
-////            case R.id.send_btn:
-////                send();
-////                break;
-////            case R.id.clear_btn:
-////                receive_show.delete(0, receive_show.length());
-////                mMessageEdt.setText(receive_show.toString());
-////                break;
-//////            case R.id.start_btn:
-//////                if(!isStartRecieveMsg) {
-//////                    initSocket();
-//////                }
-//////                break;
-////            default:
-////                break;
-////        }
-////    }
-////
-////    private void send() {
-////        String msg = mMessageEdt.getText().toString();
-////        if (mSocket.isConnected()) {
-////            if (!mSocket.isOutputShutdown()) {
-////                mWriter.println(msg);
-////            }
-////        }
-////    }
-//
-//
-//
-//}
+        //绘制坐标轴
+        canvas.drawLine(X_OFFSET, centerY, WIDTH, centerY, p); //绘制X轴
+        canvas.drawLine(X_OFFSET, 10, X_OFFSET, HEIGHT, p);    //绘制Y轴
+        holder.unlockCanvasAndPost(canvas);
+        holder.lockCanvas(new Rect(0,0,0,0));
+        holder.unlockCanvasAndPost(canvas);
+    }
+
+}
